cad1173ca2f855c6d3a3b32dc50c02e0774a7d35
nammisops <immancampos@gmail.com>
2026-03-02 11:55:40 +0800

    Protect detail view with login_required and add author to template

--- ledger/views.py
+++ ledger/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
 
-# View for the Recipe List
 def recipe_list(request):
-    # Fetch all recipes from the database
     recipes = Recipe.objects.all() 
-    
-    ctx = {
-        "recipes": recipes
-    }
+    ctx = { "recipes": recipes }
     return render(request, 'recipe_list.html', ctx)
 
-# View for the Recipe Detail (using the unique key 'pk' from the URL)
+# This decorator forces the user to log in before seeing the detail page 
+@login_required 
 def recipe_detail(request, pk):
-    # Fetch the specific recipe matching the ID in the URL
     recipe = Recipe.objects.get(id=pk) 
-    
-    ctx = {
-        "recipe": recipe
-    }
+    ctx = { "recipe": recipe }
     return render(request, 'recipe_detail.html', ctx)
